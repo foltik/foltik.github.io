b90fd18010e0c70b9c04a91b322a607cef4dd0d4
Jack Foltz <jack@foltz.io>
2023-06-21 01:00:49 -0400

    Use mailhide

--- index.js
+++ index.js
@@ -2,10 +2,4 @@ document.addEventListener('DOMContentLoaded', () => {
     // Start showcase animations from the beginning when hovering
     document.querySelectorAll('.anim').forEach(img => 
         img.addEventListener('mouseenter', () => img.src = img.src));
-
-    // Prevent simple crawlers from scraping my email.
-    const mail = 'mail';
-    const me = 'jack';
-    const domain = 'foltz.io';
-    document.querySelector('#secret').href = `${mail}to:${me}@${domain}`;
 });
